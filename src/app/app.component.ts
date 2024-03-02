3318618bbfedc7ec2cdf623ea1456b36e232e563
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-02 11:53:25 -0500

    Se Enrutan las paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './compartido/menu/menu.component';
+import { PiePaginaComponent } from './compartido/pie-pagina/pie-pagina.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, MenuComponent, PiePaginaComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+    
 })
+
+
 export class AppComponent {
   title = 'aiko-audiovisual';
+  
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { ServicioComponent } from './pagina/servicio/servicio.component';
+import { ContactoComponent } from './pagina/contacto/contacto.component';
+import { ProductoComponent } from './pagina/producto/producto.component';
+import { ProductoDetalleComponent } from './pagina/producto-detalle/producto-detalle.component';
+import { CarritoComponent } from './pagina/carrito/carrito.component';
+import { AccesoComponent } from './pagina/autenticacion/acceso/acceso.component';
+import { RegistroComponent } from './pagina/autenticacion/registro/registro.component';
+import { Error404Component } from './pagina/error404/error404.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '',
+        component:InicioComponent
+    },
+    {path: 'inicio',
+        component:InicioComponent
+    },
+    {path: 'servicio',
+        component:ServicioComponent
+    },
+    {path: 'contacto',
+        component:ContactoComponent
+    },
+    {path: 'producto',
+        component:ProductoComponent
+    },
+    {path: 'producto-detalle',
+        component:ProductoDetalleComponent
+    },
+    {path: 'carrito',
+        component:CarritoComponent
+    },
+    {path: 'acceso',
+        component:AccesoComponent
+    },
+    {path: 'registro',
+        component:RegistroComponent
+    },
+    {path: '**',
+        component: Error404Component
+    }
+];
