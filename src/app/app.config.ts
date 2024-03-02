e0127c29b67665251ee943b9ef0275e73c0a5b92
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-01 20:29:19 -0500

    Se importa Authentication / firestore de FIREBASE

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"aiko-audiovisual","appId":"1:934369803929:web:1096f0d3dc67c3ac864208","storageBucket":"aiko-audiovisual.appspot.com","apiKey":"AIzaSyBEZzAOhMx_Quprbbs-OnQoOzvUnYDyYow","authDomain":"aiko-audiovisual.firebaseapp.com","messagingSenderId":"934369803929"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
