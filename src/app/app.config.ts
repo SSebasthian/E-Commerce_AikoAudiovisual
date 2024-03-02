fcb562f7218445fb26d90922964a197818f7ac29
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-01 20:37:37 -0500

    Se hace CRUD con FIREBASE

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,33 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, getApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"aiko-audiovisual","appId":"1:934369803929:web:1096f0d3dc67c3ac864208","storageBucket":"aiko-audiovisual.appspot.com","apiKey":"AIzaSyBEZzAOhMx_Quprbbs-OnQoOzvUnYDyYow","authDomain":"aiko-audiovisual.firebaseapp.com","messagingSenderId":"934369803929"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient(),
+    provideClientHydration(), 
+    importProvidersFrom([
+      provideFirebaseApp(() => 
+        initializeApp({
+          apiKey: "AIzaSyBEZzAOhMx_Quprbbs-OnQoOzvUnYDyYow",
+          authDomain: "aiko-audiovisual.firebaseapp.com",
+          projectId: "aiko-audiovisual",
+          storageBucket: "aiko-audiovisual.appspot.com",
+          messagingSenderId: "934369803929",
+          appId: "1:934369803929:web:1096f0d3dc67c3ac864208"
+          })
+        ),
+        //configurar 
+        provideFirestore(() => getFirestore()),
+      ]), 
+      importProvidersFrom(
+        provideAuth(() => getAuth())), 
+        importProvidersFrom(provideFirestore(() => getFirestore()
+        ))
+      ]
 };
