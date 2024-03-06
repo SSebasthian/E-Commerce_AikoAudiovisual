a4a29cacc6a5920b668e3d8abd52b36fefa432ae
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-06 18:43:39 -0500

    Se Adapta Menu Responsive

--- src/app/compartido/menu/menu.component.ts
+++ src/app/compartido/menu/menu.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
@@ -7,10 +8,20 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MatIconModule, RouterLink, RouterLinkActive],
+  imports: [MatIconModule, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
+  menuAbierto: boolean = false;
+
+  cambioMenuResponsive(){
+    // SI EL CHECK DEL MENU RESPONSIVE SE OBRIME SE PASA A VERDADERO
+    // Y MUESTRA CAMBIOS DE CSS POR LA CLASE
+    this.menuAbierto= !this.menuAbierto;
+    console.log(this.menuAbierto)
+    
+  }
 
 }
