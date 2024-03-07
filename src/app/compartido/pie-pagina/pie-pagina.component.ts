26d8dd37a8c1295ceb34405259fbe1285b2948e2
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-07 18:01:49 -0500

    Se Crea Pie de Pagina Principal

--- src/app/compartido/pie-pagina/pie-pagina.component.ts
+++ src/app/compartido/pie-pagina/pie-pagina.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-pie-pagina',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './pie-pagina.component.html',
   styleUrl: './pie-pagina.component.css'
 })
