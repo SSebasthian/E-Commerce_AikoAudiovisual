1fc5050add379ce02ca5f4ef0fd31c039f4c2049
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-16 08:59:39 -0500

    Se configura CRUD con STORAGE - FireBase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { initializeApp, getApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStorage, getStorage } from '@angular/fire/storage'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,13 +20,16 @@ export const appConfig: ApplicationConfig = {
           apiKey: "AIzaSyBEZzAOhMx_Quprbbs-OnQoOzvUnYDyYow",
           authDomain: "aiko-audiovisual.firebaseapp.com",
           projectId: "aiko-audiovisual",
-          storageBucket: "aiko-audiovisual.appspot.com",
+          storageBucket: "gs://aiko-audiovisual.appspot.com",
           messagingSenderId: "934369803929",
           appId: "1:934369803929:web:1096f0d3dc67c3ac864208"
           })
         ),
-        //configurar 
+        //configura componentes de Firestore 
         provideFirestore(() => getFirestore()),
+
+        //configura componentes de Storage
+        provideStorage(() => getStorage()),
       ]), 
       importProvidersFrom(
         provideAuth(() => getAuth())), 
