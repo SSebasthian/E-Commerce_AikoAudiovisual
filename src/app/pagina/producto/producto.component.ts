785b6b54e9a8db8e864cde7b48ef225c0a218b24
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-19 21:49:48 -0500

    Se Diseña Pagina Productos y Se Llaman Productos INGRESADOS a Firebase

--- src/app/pagina/producto/producto.component.ts
+++ src/app/pagina/producto/producto.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import Producto from '../../arquitectura/interface/producto.interface';
+import { ProductoService } from '../../arquitectura/servicio/producto.service';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+  ],
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.css'
 })
 export class ProductoComponent {
 
+  productos: Producto[];
+
+  constructor(
+    private productoservicio: ProductoService,
+  ){
+      this.productos = [{
+      categoria: '',
+      titulo:'',
+      stock:0,
+      precio: 0,
+      descripcion:'',
+      imagen: '',
+      fecha: new Date(),
+    }];
+  }
+  ngOnInit(): void {
+    this.productoservicio.obtenerProducto().subscribe(producto => {
+      this.productos = producto;
+    })
+  }
 }
