2bc5517bb6f661510f406bb573a37ea9e42371fc
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-23 17:00:37 -0500

    Se Diseña Pagina Producto-Detalle

--- src/app/pagina/producto-detalle/producto-detalle.component.ts
+++ src/app/pagina/producto-detalle/producto-detalle.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import Producto from '../../arquitectura/interface/producto.interface';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms'; 
+import { RouterLink } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-producto-detalle',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    FormsModule,
+    RouterLink
   ],
   templateUrl: './producto-detalle.component.html',
   styleUrl: './producto-detalle.component.css'
 })
 export class ProductoDetalleComponent {
   productoSeleccionado: Producto | null = null;
+  cantidad: number = 1; // Inicializa la cantidad en 1
 
   ngOnInit(): void {
     // Obtiene el producto seleccionado del localStorage
@@ -21,6 +29,27 @@ export class ProductoDetalleComponent {
     if (productoSeleccionadoJSON) {
       this.productoSeleccionado = JSON.parse(productoSeleccionadoJSON);
       console.log('Producto Seleccionado:', this.productoSeleccionado);
+    }
+  }
+
+  // ESTE METODO INGREMENTA LA CANTIDAD EN EL ARTICULO
+  incrementarCantidad() {
+    if (this.productoSeleccionado && this.cantidad < this.productoSeleccionado.stock) {
+      this.cantidad++;
+    }
+  }
+
+  // ESTE METODO DISMINUYE LA CANTIDAD EN EL ARTICULO
+  decrementarCantidad() {
+    if (this.cantidad > 1) {
+      this.cantidad--;
+    }
+  }
+  
+  // ESTE METODO VALIDA LA CANTIDAD EN EL ARTICULO Y NO PERMITE SUPERAR ESTE STOCK
+  validarCantidad() {
+    if (this.productoSeleccionado && this.cantidad > this.productoSeleccionado.stock) {
+      this.cantidad = this.productoSeleccionado.stock; // Ajusta la cantidad al máximo stock disponible
     }
   }
 
