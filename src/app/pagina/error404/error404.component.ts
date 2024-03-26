1f1d26e1547161fd3063d61ab1b3daec1e7b6048
SSebasthian <ssebasthian.perez@gmail.com>
2024-03-26 15:50:51 -0500

    Se Diseña Pagira Error 404

--- src/app/pagina/error404/error404.component.ts
+++ src/app/pagina/error404/error404.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error404',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    CommonModule
+  ],
   templateUrl: './error404.component.html',
   styleUrl: './error404.component.css'
 })
